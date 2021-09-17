a13fc29c7ce3af4ee216f3eaec0bf744f5cc6bc0
wuyuhong <wuyuhong@ihuman.com>
2021-09-17 14:55:40 +0800

    init project

---
+++ TestEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wynncs
+{
+    public class TestEntry
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello, world");
+        }
+    }
+}
